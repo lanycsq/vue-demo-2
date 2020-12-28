527fe5eaae1ca6468d361a6761af67c506ab2136
lanycsq <shanqiong118@163.com>
2020-12-28 18:33:31 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,114 @@
 <template>
   <div id="app">
-    <div class="im">
-      <input v-model="message" placeholder="edit me" />
-      <p>Message is: {{ message }}</p>
-      <p>
-        <button @click="message = 'frank'">set message to frank</button>
-      </p>
-    </div>
-    <div class="checkedNames">
-      <input type="checkbox" id="jack" value="Jack" v-model="checkedNames" />
-      <label for="jack">Jack</label>
-      <input type="checkbox" id="john" value="John" v-model="checkedNames" />
-      <label for="john">John</label>
-      <input type="checkbox" id="mike" value="Mike" v-model="checkedNames" />
-      <label for="mike">Mike</label>
-      <br />
-      <span>Checked names: {{ checkedNames }}</span>
-    </div>
-    <div id="example-4">
-      <input type="radio" name="want" id="one" value="One" v-model="picked" />
-      <label for="one">One</label>
-      <br />
-      <input type="radio" name="want" id="two" value="Two" v-model="picked" />
-      <label for="two">Two</label>
-      <br />
-      <span>Picked: {{ picked }}</span>
-    </div>
-    <div id="example-5">
-      <select multiple v-model="selected">
-        <option value="">请选择</option>
-        <option v-for="item in array" :value="item.value" :key="item.value">
-          {{ item.text }}
-        </option>
-      </select>
-      <span>Selected: {{ selected }}</span>
-    </div>
+<<<<<<< Updated upstream
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+=======
+    <a-form
+      id="components-form-demo-normal-login"
+      :form="form"
+      class="login-form"
+      @submit="handleSubmit"
+    >
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'username',
+            {
+              rules: [
+                { required: true, message: 'Please input your username!' },
+              ],
+            },
+          ]"
+          placeholder="username"
+        >
+          <a-icon
+            slot="prefix"
+            type="user"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-input
+          v-decorator="[
+            'password',
+            {
+              rules: [
+                { required: true, message: 'Please input your Password!' },
+              ],
+            },
+          ]"
+          type="password"
+          placeholder="password"
+        >
+          <a-icon
+            slot="prefix"
+            type="lock"
+            style="color: rgba(0, 0, 0, 0.25)"
+          />
+        </a-input>
+      </a-form-item>
+      <a-form-item>
+        <a-checkbox
+          v-decorator="[
+            'remember',
+            {
+              valuePropName: 'checked',
+              initialValue: true,
+            },
+          ]"
+        >
+          Remember me
+        </a-checkbox>
+        <a class="login-form-forgot" href=""> Forgot password </a>
+        <a-button type="primary" html-type="submit" class="login-form-button">
+          Log in
+        </a-button>
+        Or
+        <a href=""> register now! </a>
+      </a-form-item>
+    </a-form>
+>>>>>>> Stashed changes
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
+<<<<<<< Updated upstream
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+=======
   name: "App",
-  data() {
-    return {
-      message: "hi",
-      checkedNames: [],
-      picked: "",
-      selected: "",
-      array: [
-        { text: "抽烟", value: 1 },
-        { text: "喝酒", value: 2 },
-        { text: "烫头", value: 3 },
-        { text: "挤痘痘", value: 4 },
-      ],
-    };
+  beforeCreate() {
+    this.form = this.$form.createForm(this, { name: "normal_login" });
+  },
+  methods: {
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log("Received values of form: ", values);
+        }
+      });
+    },
   },
 };
+>>>>>>> Stashed changes
 </script>
 
-
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
