dc5d16836bc601b3df4f0eedb4cb74f1dbcb3f3b
lanycsq <shanqiong118@163.com>
2020-12-28 18:35:07 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,19 @@
 <template>
   <div id="app">
-<<<<<<< Updated upstream
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-=======
-    <a-form
-      id="components-form-demo-normal-login"
-      :form="form"
-      class="login-form"
-      @submit="handleSubmit"
-    >
-      <a-form-item>
-        <a-input
-          v-decorator="[
-            'username',
-            {
-              rules: [
-                { required: true, message: 'Please input your username!' },
-              ],
-            },
-          ]"
-          placeholder="username"
-        >
-          <a-icon
-            slot="prefix"
-            type="user"
-            style="color: rgba(0, 0, 0, 0.25)"
-          />
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-input
-          v-decorator="[
-            'password',
-            {
-              rules: [
-                { required: true, message: 'Please input your Password!' },
-              ],
-            },
-          ]"
-          type="password"
-          placeholder="password"
-        >
-          <a-icon
-            slot="prefix"
-            type="lock"
-            style="color: rgba(0, 0, 0, 0.25)"
-          />
-        </a-input>
-      </a-form-item>
-      <a-form-item>
-        <a-checkbox
-          v-decorator="[
-            'remember',
-            {
-              valuePropName: 'checked',
-              initialValue: true,
-            },
-          ]"
-        >
-          Remember me
-        </a-checkbox>
-        <a class="login-form-forgot" href=""> Forgot password </a>
-        <a-button type="primary" html-type="submit" class="login-form-button">
-          Log in
-        </a-button>
-        Or
-        <a href=""> register now! </a>
-      </a-form-item>
-    </a-form>
->>>>>>> Stashed changes
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-<<<<<<< Updated upstream
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-=======
   name: "App",
-  beforeCreate() {
-    this.form = this.$form.createForm(this, { name: "normal_login" });
-  },
-  methods: {
-    handleSubmit(e) {
-      e.preventDefault();
-      this.form.validateFields((err, values) => {
-        if (!err) {
-          console.log("Received values of form: ", values);
-        }
-      });
-    },
+  components: {
+    HelloWorld,
   },
 };
->>>>>>> Stashed changes
 </script>
 
 <style lang="scss">
